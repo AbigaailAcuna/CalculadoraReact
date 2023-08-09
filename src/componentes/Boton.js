7d0b21110a919bb0e4dc465e2a9a482c99f2cc50
AbigaailAcuna <acuaabigail@yahoo.com>
2023-08-08 21:59:23 -0600

    generalidades de calculadora

---
+++ src/componentes/Boton.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+//la clase boton contenedor será fija para todos pero si es num o si es operador se debe evaluar
+function Boton(props) {
+
+	const esOperador = valor =>{
+		return isNaN(valor) && (valor != '.') && (valor!= '=');
+	}
+    return(
+			<div className={`boton-contenedor ${esOperador(props.children) ? 'operador' : ''}`.trimEnd()}>
+				{props.children}
+			</div>
+    );
+
+}
+
+export default Boton;
